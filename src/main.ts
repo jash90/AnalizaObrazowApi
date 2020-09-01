d197dd315cc8025e84673c8fe5603f9e572a54ba
Bartlomiej Zimny <b.zimny@getprintbox.com>
2020-09-01 19:44:04 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger';
+import config from '../config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors();
     setupSwagger(app);
+    console.log(config);
     await app.listen(3091);
 }
 
